1a558a52bbb129fe600f75b228dd3fe9a632b71d
VAB1998 <blanco.vincenzo@outlook.com>
2021-09-29 16:17:20 +0200

    Installed and imported Axios

--- src/components/Albums.vue
+++ src/components/Albums.vue
@@ -8,12 +8,31 @@
 
 <script>
 import Album from './Album.vue'
-
+import axios from 'axios'
 export default {
     name: 'Albums',
+
     components: {
         Album
-    }
+    },
+
+    data : function(){
+        return{
+
+        }
+    },
+
+    created : function() {
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then((object) =>{
+            console.clear()
+            console.log(object)
+            console.log(object.data)
+            console.log(object.data.response)
+            console.log(object.data.response[0])
+            console.log(object.data.response[0].author)
+        })
+    } 
 }
 </script>
 
