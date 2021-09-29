50dff897dae42d4f3c2bd40c0324708f40d80045
VAB1998 <blanco.vincenzo@outlook.com>
2021-09-29 16:45:57 +0200

    Added v-for directive to render the list of albums

--- src/components/Albums.vue
+++ src/components/Albums.vue
@@ -1,8 +1,8 @@
 <template>
     <section id="albums">
         <div class="container">
-            <Album />
-            <h1>{{title}}</h1>
+            <Album  v-for="(item, index) in albumList" :key="index"/>
+            
         </div>
     </section>
 </template>
@@ -30,7 +30,7 @@ export default {
         axios.get('https://flynn.boolean.careers/exercises/api/array/music')
         .then((object) =>{
             console.clear()
-
+            //Take the Datas from the API and put them into the array (using slice)
             this.albumList =  object.data.response.slice()
             
             //Check
